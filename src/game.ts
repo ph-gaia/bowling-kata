91c6952a8dd4ecf7281641c2f485882f18c10312
Paulo Henrique <phcgaia11@yahoo.com.br>
2021-08-31 23:35:05 -0300

    feat: spare score

--- src/game.ts
+++ src/game.ts
@@ -5,9 +5,14 @@ export class Game {
   score(): number {
     let score = 0
     let i = 0
-    for(let frame = 0; frame < 10; frame++) {
-      score += this.rolls[i] + this.rolls[i + 1]
-      i += 2
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.rolls[i] + this.rolls[i + 1] === 10) { // spare
+        score += 10 + this.rolls[i + 2]
+        i += 2
+      } else {
+        score += this.rolls[i] + this.rolls[i + 1]
+        i += 2
+      }
     }
     return score
   }
